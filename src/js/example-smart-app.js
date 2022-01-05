4e9ae7b835aa58d1551f20c34b6a1ee63d25be2c
gshraddha <52274668+gshraddha@users.noreply.github.com>
2022-01-05 09:34:58 -0500

    Updated

--- src/js/example-smart-app.js
+++ src/js/example-smart-app.js
@@ -22,9 +22,9 @@
                     }
                   });
 
-        $.when(pt, obv).fail(onError);
+        $.when(pt).fail(onError);
 
-        $.when(pt, obv).done(function(patient, obv) {
+        $.when(pt).done(function(patient) {
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
 
